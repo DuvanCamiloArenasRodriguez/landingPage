8f782a62b39debdc934f11f1da8384bd76635219
DuvanCamiloArenasRodriguez <dccar21@gmail.com>
2024-01-18 20:52:08 -0500

    cambio a movil el menu (responsive design)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-// AOS.init({
-//     offset:1
-// });
+AOS.init({
+    offset:1
+});
+
+let links = document.querySelector('.links')
+
+document.querySelector('.boton').addEventListener('click', function(){
+    links.classList.toggle('active')
+})
